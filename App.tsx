7b08f373dcba8c4ab5cfc336a33679529999dfdd
FerCamposDev <fernando.developments@gmail.com>
2023-04-16 23:58:56 -0300

    native base working

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box, Text, extendTheme, ColorMode } from "native-base";
+import type { StorageManager } from "native-base";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import ThemeExample from "./src/Screens/ThemeExample";
+
+const newColorTheme = {
+  brand: {
+    900: "#8287af",
+    800: "#7c83db",
+    700: "#b3bef6",
+  },
+};
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'dark',
+};
+
+const theme = extendTheme({ colors: newColorTheme });
 
 export default function App() {
+  const colorModeManager: StorageManager = {
+    get: async () => {
+      try {
+        let mode = await AsyncStorage.getItem("@my-app-color-mode");
+        return mode === "dark" ? "dark" : "light";
+      } catch (e) {
+        console.log(e);
+        return 'light';
+      }
+    },
+    set: async (value: ColorMode) => {
+      try {
+        if (value) {
+          await AsyncStorage.setItem("@my-app-color-mode", value);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>My App!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <ThemeExample />
+      {/* <Box flex={1} alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box> */}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
