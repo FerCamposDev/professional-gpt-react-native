162dd1a18e003d7adc0691afe92cb04b810fc0eb
fercamposdev <fernando.developments@gmail.com>
2023-04-17 20:25:34 -0300

    some changes

--- src/Components/APIKeyModal.tsx
+++ src/Components/APIKeyModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, Text } from "native-base"
+import { Alert, Box, Button, HStack, Link, Modal, Text, VStack } from "native-base"
 import { useState } from "react";
 
 type Props = {}
@@ -11,17 +11,33 @@ const APIKeyModal = (props: Props) => {
       <Button variant="outline" onPress={() => setOpen(true)}>
         Como obtener mi API Key?
       </Button>
-      <Modal isOpen={open} onClose={setOpen}>
+      <Modal isOpen={open} onClose={() => setOpen(false)} size="lg">
         <Modal.Content>
           <Modal.CloseButton />
-          <Modal.Header>Como obtener mi API Key</Modal.Header> 
-          
+          <Modal.Header>Como obtener mi API Key?</Modal.Header>
+
           <Modal.Body>
-            <Text>My modal</Text>
+            <VStack space={2}>
+              <Text>
+                Para poder obtener su api key es necesario tener una cuenta en <Link href="https://platform.openai.com">OpenAI</Link>
+              </Text>
+              <Text>
+                Una vez creada la cuenta debe acceder a <Link href="https://platform.openai.com/account/api-keys">Accounts/api-keys</Link>
+              </Text>
+              <Alert status="warning" colorScheme="warning">
+                <HStack flexShrink={1} space={2} alignItems="center">
+                  <Alert.Icon />
+                  <Text>
+                    Recuerde copiar su clave antes de cerrar la ventana porque no podra visualizarla nuevamente.
+                    Puede crear y eliminar tantas keys como usted quiera.
+                  </Text>
+                </HStack>
+              </Alert>
+            </VStack>
           </Modal.Body>
 
           <Modal.Footer>
-            <Button onPress={()=> setOpen(false)}>
+            <Button onPress={() => setOpen(false)}>
               Entendido
             </Button>
           </Modal.Footer>
--- src/Screens/Config.tsx
+++ src/Screens/Config.tsx
@@ -1,13 +1,23 @@
-import { Heading, Button, Text, VStack } from "native-base"
+import { Heading, Button, Text, VStack, Alert, HStack } from "native-base"
 import APIKeyModal from "../Components/APIKeyModal"
 
 type Props = {}
 
 const Config = (props: Props) => {
   return (
-    <VStack>
+    <VStack padding={4} space={4}>
       <Heading textAlign="center">Bienvenido a Professional AI</Heading>
-      <Text>Para hacer funcionar la app necesitas proporsionar un API Key(codigo de acceso a chat GPT), solo se almacenara en tu dispositivo y sera utilizado por ti.</Text>
+      <Text>
+        Para hacer funcionar la app necesitas proporsionar un API Key(codigo de acceso a chat GPT), solo se almacenara en tu dispositivo y sera utilizado por ti.
+      </Text>
+      <Alert status="info">
+        <HStack space={2} alignItems="center" paddingX={6}>
+          <Alert.Icon />
+          <Text textAlign="justify">
+            Necesitamos hacer esto ya que usar inteligencia artificial tiene un costo el cual no podemos cubrir para todos los usuarios, al cargar su propia key usted podra utilizar la aplicacion hasta agotar la cantidad de consultas mensuales gratis.
+          </Text>
+        </HStack>
+      </Alert>
       <APIKeyModal />
     </VStack>
   )
